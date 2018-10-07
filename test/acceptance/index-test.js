dba9d33105a998e7eaf095cad0018d0a0f68fdd5
Kelly Selden <kellyselden@gmail.com>
2018-10-07 11:59:05 +0100

    node 6 syntax

--- test/acceptance/index-test.js
+++ test/acceptance/index-test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const cp = require('child_process');
 
 describe('acceptance', function() {
--- test/unit/_index-test.js
+++ test/unit/_index-test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const debug = require('../../src/_index');
 
 describe('unit', function() {
